e60aa63420872152c0455013341eba09018cb985
Adwait Samak <88927689+AdwaitSamak@users.noreply.github.com>
2024-01-19 06:48:18 +0000

    Commit on test.js

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
 console.log("Hi");
+
+let a ="Adwait";
+let b=33;
+console.table([a,b]);
+
+a=Number(a);
+b=String(b);
+console.table([a,b]);
+
+// "123" to number=> 123
+// "11aa" to number => NaN
+
+// "1"+2+2 => "122"
+// 1+2+"2" => "32"
